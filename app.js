e2a1242ef40fffb3499d7eb6505bb05891076ca6
Rubén Darío Guardo de La Rosa <112723142+JuggernautRG08@users.noreply.github.com>
2024-09-20 20:12:32 -0500

    Adición Swagger, Conexión BD MongoDb

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const institucionRoutes = require('./routes/institucionRoutes');
+const connectDB = require('../api-instituciones/config/db');
+const swaggerDocs = require('../api-instituciones/config/swagger'); 
 
 dotenv.config();
+const app = express();
 
 // Conectar a la base de datos
 connectDB();
 
-const app = express();
+// Middleware para analizar JSON
 app.use(express.json());
 
-// Rutas
+// Definir rutas
+const institucionRoutes = require('../api-instituciones/routes/institucionRoutes');
 app.use('/api/instituciones', institucionRoutes);
 
-const PORT = process.env
+// Iniciar Swagger en el puerto 5000
+swaggerDocs(app);
+
+// Definir puerto
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
--- config/db.js
+++ config/db.js
@@ -4,14 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Conectado a la base de datos MongoDB');
     } catch (error) {
         console.error('Error al conectar a la base de datos', error);
-        process.exit(1); // Detener la ejecución si hay error en la conexión
+        process.exit(1); 
     }
 };
 
